f946b73401a9ac34ac4c4abf2e765523f7f2b13a
francis-mwakidoshi <francismwakidoshi@gmail.com>
2017-04-02 00:40:15 +0300

    creating an alert welcome box

--- index.php
+++ index.php
@@ -690,9 +690,9 @@
         $(document).ready(function () {
         var unique_id = $.gritter.add({
             // (string | mandatory) the heading of the notification
-            title: 'Welcome to Dashgum!',
+            title: 'Welcome to smart-parking!',
             // (string | mandatory) the text inside the notification
-            text: 'Hover me to enable the Close Button. You can hide the left sidebar clicking on the button next to the logo. Free version for <a href="http://blacktie.co" target="_blank" style="color:#ffd777">BlackTie.co</a>.',
+            text: 'Makes your parking easier.'
             // (string | optional) the image to display on the left
             image: 'assets/img/ui-sam.jpg',
             // (bool | optional) if you want it to fade out on its own or just sit there
