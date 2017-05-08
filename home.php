c3d037d9375bccf169548513bdb4388e2808d1ae
NdauwaRafael <raphaelndauwa@gmail.com>
2017-05-08 13:09:50 +0300

    home page design home.php where customers will be able to book their favourite parking spot

--- home.php
+++ home.php
@@ -12,6 +12,75 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <link href="assets/css/bootstrap.css" rel="stylesheet">
+    <style>
+        body {
+          font-family: "Open Sans Condensed", sans-serif;
+          background: url(assets/img/1.jpg) no-repeat center center fixed;
+          background-size: cover;
+        }
+
+        .top {
+          width: 90%;
+          margin: 0px auto;
+          color:#bbdefb !important;
+
+        }
+        .bg {
+          background: rgba(130,130,130,.7);
+          border-bottom: 1px solid #eeff41;
+        }
+
+        .searchbox {
+           background: rgba(130,130,130,.7);
+          padding: 4% 10px 8%;
+          color:#bbdefb !important;
+        }
+
+.area{
+  margin-bottom: 15px;
+}
+
+    </style>
+</head>
+<body>
+<nav class="row bg">
+<div class="top">
+  <div class="col-md-4"><h1>Smart Parking </h1></div>
+  <div class="col-md-4"></div>
+  <div class="col-md-4"><br><h4><span class="glyphicon glyphicon-user"></span>Loggedin User</h4></div>
+  </div>
+</nav>
+
+<div class="row">
+   <div class="container">
+
+   <div class="col-md-6 searchbox">
+          <div class="page-header">
+            <h1>Find A Parking Spot<br></h1>
+            <small>Search for a parking spot near you</small>
+          </div>
+      <form >
+
+    <div class="input-group area">
+      <input type="text" class="form-control" placeholder="Find by Address, Neighborhood or Destination">
+      <span class="input-group-btn">
+        <button class="btn btn-danger" type="button">SEARCH NOW!</button>
+      </span>
+    </div><!-- /input-group -->
+
+        <label class="radio-inline">
+          <input type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1">DAILY PARKING
+        </label>
+        <label class="radio-inline">
+          <input type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2"> MONTHLY PARKING
+        </label>
+      </form>
+   </div>
+
+   <div class="col-md-6">.col-md-4</div>
+   </div>
+</div>
+
 
 
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
