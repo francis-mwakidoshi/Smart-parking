35a4772b243d1da6566320d18e4dd075d72d21d0
NdauwaRafael <raphaelndauwa@gmail.com>
2017-06-02 18:23:44 +0300

    REservation space modal design

--- home.php
+++ home.php
@@ -64,6 +64,10 @@ else {
   color: #2F4F4F !important;
   text-transform: uppercase;
 }
+
+.total {
+  color: #FF0000 !important;
+}
     </style>
 </head>
 <body>
@@ -138,6 +142,7 @@ while ($parking = mysqli_fetch_array($parkings_result)) {
   $parking_street = $parking['street'];
   $parking_name = $parking['name'];
   $parking_slot = $parking['slot'];
+  $parking_price = $parking['price'];
 ?>
 <div class="panel panel-default parking_text">
   <div class="panel-body">
@@ -149,8 +154,46 @@ while ($parking = mysqli_fetch_array($parkings_result)) {
       <li class="list-group-item"><span class="glyphicon glyphicon-map-marker"></span> <?=$parking_street; ?></li>
       <li class="list-group-item"><span class="glyphicon glyphicon-tags"></span> <?=$parking_slot; ?></li>
     </ul>
-    <button class="btn btn-default" type="submit">select Now!!</button>
+    <button class="btn btn-default" type="button" data-toggle="modal" data-target="#reserve<?=$parking_id ; ?>">select Now!!</button>
 
+  </div>
+</div>
+
+<div class="modal fade parking_text" id="reserve<?=$parking_id ; ?>" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="myModalLabel"><?=$parking_name; ?> Reservation</h4>
+      </div>
+      <div class="modal-body">
+        <ul class="list-group">
+          <li class="list-group-item"><span class="glyphicon glyphicon-home"></span> <?=$parking_name; ?></li>
+          <li class="list-group-item"><span class="glyphicon glyphicon-tags"></span> <?=$parking_slot; ?> total slots </li>
+          <li class="list-group-item"><span class="glyphicon glyphicon-tag"></span> <?=$parking_slot; ?> Remaining Slots</li>
+          <li class="list-group-item"><span class="glyphicon glyphicon-credit-card"></span> Ksh. <?=$parking_price; ?> Per Slot Per Hour</li>
+          <li class="list-group-item total"> Ksh. <?=$parking_slot; ?> Total cost</li>
+          <li class="list-group-item">
+            <div class="input-group">
+              <input type="text" class="form-control" placeholder="Number of slots" aria-describedby="basic-addon2">
+              <span class="input-group-addon" id="basic-addon2">Slots</span>
+            </div>
+          </li>
+          <li class="list-group-item">
+            <div class="input-group">
+              <input type="text" class="form-control" placeholder="Number of Hours" aria-describedby="basic-addon2">
+              <span class="input-group-addon" id="basic-addon2">Hours</span>
+            </div>
+          </li>
+
+
+        </ul>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Select this space</button>
+      </div>
+    </div>
   </div>
 </div>
 <?php
